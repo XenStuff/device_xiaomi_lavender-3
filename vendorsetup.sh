dc4537e806e1e865aeb44e54e61fcd443c37ba37
MRMDprjkt° <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-08-07 13:16:41 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-git clone --depth=1  https://github.com/stormbreaker-project/kernel_xiaomi_lavender kernel/xiaomi/lavender
-git clone --depth=1 https://github.com/XenStuff/vendor_xiaomi_lavender-2 vendor/xiaomi/lavender
+git clone --depth=1  https://github.com/Merma1dmanPrjct/kernel_xiaomi_lavender -b backup-9.9 kernel/xiaomi/lavender
+git clone --depth=1 https://github.com/Merma1dmanPrjct/android_vendor_xiaomi_lavender-1 vendor/xiaomi/lavender

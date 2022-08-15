9a9720f132cc4edafbfe3a9ec5b5575b6f2cb6dd
MRMDprjkt° <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-08-16 00:38:31 +0700

    Update nad_lavender.mk

--- nad_lavender.mk
+++ nad_lavender.mk
@@ -13,13 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Corvus stuff.
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 #USE_GAPPS ?= true 
 #TARGET_GAPPS_ARCH := arm64 
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nad_lavender
+PRODUCT_NAME := du_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -31,5 +31,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Nusa Flags
-NAD_BUILD_TYPE := GAMING
+
+DU_BUILD_TYPE := GAMING
